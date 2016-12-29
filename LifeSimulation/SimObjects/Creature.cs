8c00c2e78fa8ab1527e13767a0e35a7c7c75a211
Johan Vergeer <johanvergeer@gmail.com>
2016-12-29 14:58:43 +0100

    Update Creature class. Work in progress

--- LifeSimulation/SimObjects/Creature.cs
+++ LifeSimulation/SimObjects/Creature.cs
@@ -1,4 +1,7 @@
-﻿namespace LifeSimulation.SimObjects
+﻿using LifeSimulation.Layouts;
+using System.Collections.Generic;
+
+namespace LifeSimulation.SimObjects
 {
     public class Creature : SimObject
     {
@@ -16,13 +19,42 @@
         }
 
         /// <summary>
+        /// Make a creature perform an action
+        /// 
+        /// The creature performs an action.
+        /// </summary>
+        /// <param name="layout">The entire layout a creature is in</param>
+        /// <param name="simObjects">List of SimObjects in the direct environment</param>
+        /// <param name="gridSizeX">X size of the grid</param>
+        /// <param name="gridSizeY">Y size of the grid</param>
+        public void Act(Layout layout, List<SimObject> simObjects)
+        {
+
+        }
+
+
+        /// <summary>
         /// The creature can move to a given location on the grid
         /// </summary>
         /// <param name="xPos">new xPos of the creature on the grid</param>
         /// <param name="yPos">new yPos of the creature on the grid</param>
-        public void Move(int xPos, int yPos)
+        public void Move(Layout layout, List<Obstacle> obstacles, int GridSizeX, int GridSizeY)
         {
+            
+        }
 
+        /// <summary>
+        /// Used to check if a creature collides every time it moves to another piece of the grid.
+        /// 
+        /// 
+        /// </summary>
+        /// <returns>
+        /// true if the creature can continue
+        /// false if the creature has to pick another direction
+        /// </returns>
+        private bool CheckCollision()
+        {
+            return true;
         }
 
         /// <summary>
@@ -30,7 +62,7 @@
         /// </summary>
         /// <param name="creature">The creature this creature will mate with</param>
         /// <exception cref="">Thrown if the creature in the input parameter is not of the same species</exception>
-        public void Mate(Creature creature)
+        private void Mate(Creature creature)
         {
 
         }
@@ -40,7 +72,7 @@
         /// </summary>
         /// <param name="simObject">Sim object that will be eaten by the creature</param>
         /// <exception cref="">Thrown if the eaten simObject does not match the creatures digestions</exception>
-        public void Eat(SimObject simObject)
+        private void Eat(SimObject simObject)
         {
 
         }
