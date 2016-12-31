4fd0998c251d296d1d1a5e9a5d8ad323fa3cdccc
Johan Vergeer <johanvergeer@gmail.ccom>
2016-12-31 16:09:29 +0100

    Implementation of Species class

---
+++ LifeSimulation/Exceptions/InvalidNumberOfLegsException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSimulation.Exceptions
+{
+    class InvalidNumberOfLegsException : Exception
+    {
+        public InvalidNumberOfLegsException()
+        {
+        }
+
+        public InvalidNumberOfLegsException(string message) : base(message)
+        {
+        }
+
+        public InvalidNumberOfLegsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidNumberOfLegsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- LifeSimulation/SimObjects/Species.cs
+++ LifeSimulation/SimObjects/Species.cs
@@ -1,22 +1,101 @@
-﻿namespace LifeSimulation.SimObjects
+﻿using System;
+using LifeSimulation.Exceptions;
+
+namespace LifeSimulation.SimObjects
 {
     public class Species
     {
+        private int _nLegs;
+        private int _stamina;
+        private int _searing;
+        private int _movingThreshold;
+        private int _swimmingThreshold;
+        private int _reproductionCosts;
+
         public string Name { get; set; }
-        public int Searing { get; set; }
-        public int NLegs { get; set; }
+
+        public int Stamina
+        {
+            get { return _stamina; }
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException(nameof(value));
+                _stamina = value;
+            }
+        }
+
+        public int NLegs
+        {
+            get { return _nLegs; }
+            set
+            {
+                if (value == 0 || value % 4 != 0)
+                    throw new InvalidNumberOfLegsException();
+                _nLegs = value;
+            }
+        }
+
         public Digestion Digestion { get; set; }
-        public int MovingThreshold { get; set; }
-        public int SwimmingThreshold { get; set; }
-        public int ReproductionCosts { get; set; }
-        public int Stamina { get; set; }
+
+        public int Searing
+        {
+            get
+            {
+                return _searing;
+            }
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException(nameof(value));
+                _searing = value;
+            }
+        }
+
+        public int MovingThreshold
+        {
+            get { return _movingThreshold; }
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException(nameof(value));
+                _movingThreshold = value;
+            }
+        }
+
+        public int SwimmingThreshold
+        {
+            get { return _swimmingThreshold; }
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException(nameof(value));
+                _swimmingThreshold = value;
+            }
+        }
+
+        public int ReproductionCosts
+        {
+            get { return _reproductionCosts; }
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException(nameof(value));
+                _reproductionCosts = value;
+            }
+        }
+
         public int Herbehaviour { get; set; }
-        public int MinimumWeight { get; set; }
+
+        public int MinimumWeight => NLegs * 10;
 
         public Species(string name, int searing, int nLegs, Digestion digestion, int movingThreshold, int swimmingThreshold,
-            int reproductionCosts, int stamina, int herdBehaviour, int minimumweight)
+            int reproductionCosts, int stamina, int herdBehaviour)
         {
-
+            Name = name;
+            Stamina = stamina;
+            NLegs = nLegs;
+            Searing = searing;
+            Digestion = digestion;
+            MovingThreshold = movingThreshold;
+            SwimmingThreshold = swimmingThreshold;
+            ReproductionCosts = reproductionCosts;
+            Herbehaviour = herdBehaviour;
         }
     }
 }
