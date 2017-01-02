d9c131095cc02b0a6e5d5de8d07be29f775a5369
Johan Vergeer <johanvergeer@gmail.ccom>
2017-01-02 19:37:31 +0100

    Add unittests base

---
+++ LifeSimulationTests/SimObjects/CreatureTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LifeSimulation.SimObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSimulation.SimObjects.Tests
+{
+    [TestClass()]
+    public class CreatureTests
+    {
+        [TestMethod()]
+        public void ActTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
