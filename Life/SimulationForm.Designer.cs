ede1363c29a4afe6a9cfd12707a3fdc28f703ed2
FlorisBoschFontys <floris.bosch@student.fontys.nl>
2017-01-04 20:29:46 +0100

    Picturebox gebruiken voor tekenen pauze en start knoppen maken, Alle simubojects tekenen

--- Life/SimulationForm.Designer.cs
+++ Life/SimulationForm.Designer.cs
@@ -31,66 +31,64 @@
             this.btnStartPause = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.panelBottom = new System.Windows.Forms.Panel();
-            this.simulationPanel = new System.Windows.Forms.Panel();
+            this.simulationPanel = new System.Windows.Forms.PictureBox();
             this.panelBottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.simulationPanel)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStartPause
             // 
-            this.btnStartPause.Location = new System.Drawing.Point(16, 4);
-            this.btnStartPause.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStartPause.Location = new System.Drawing.Point(12, 3);
             this.btnStartPause.Name = "btnStartPause";
-            this.btnStartPause.Size = new System.Drawing.Size(100, 28);
+            this.btnStartPause.Size = new System.Drawing.Size(75, 23);
             this.btnStartPause.TabIndex = 1;
             this.btnStartPause.Text = "Start";
             this.btnStartPause.UseVisualStyleBackColor = true;
+            this.btnStartPause.Click += new System.EventHandler(this.btnStartPause_Click);
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(124, 4);
-            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStop.Location = new System.Drawing.Point(93, 3);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(100, 28);
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
             this.btnStop.TabIndex = 2;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // panelBottom
             // 
             this.panelBottom.Controls.Add(this.btnStartPause);
             this.panelBottom.Controls.Add(this.btnStop);
             this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelBottom.Location = new System.Drawing.Point(0, 265);
-            this.panelBottom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelBottom.Location = new System.Drawing.Point(0, 215);
             this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(727, 43);
+            this.panelBottom.Size = new System.Drawing.Size(545, 35);
             this.panelBottom.TabIndex = 3;
             // 
             // simulationPanel
             // 
-            this.simulationPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.simulationPanel.BackColor = System.Drawing.Color.RoyalBlue;
             this.simulationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.simulationPanel.Location = new System.Drawing.Point(0, 0);
-            this.simulationPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.simulationPanel.Name = "simulationPanel";
-            this.simulationPanel.Size = new System.Drawing.Size(727, 308);
-            this.simulationPanel.TabIndex = 0;
-            this.simulationPanel.SizeChanged += new System.EventHandler(this.simulationPanel_SizeChanged);
-            this.simulationPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.simulationPanel_Paint);
+            this.simulationPanel.Size = new System.Drawing.Size(545, 215);
+            this.simulationPanel.TabIndex = 4;
+            this.simulationPanel.TabStop = false;
+            this.simulationPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // SimulationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 308);
-            this.Controls.Add(this.panelBottom);
+            this.ClientSize = new System.Drawing.Size(545, 250);
             this.Controls.Add(this.simulationPanel);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.panelBottom);
             this.Name = "SimulationForm";
             this.Text = "Simulatie";
             this.Resize += new System.EventHandler(this.SimulationForm_Resize);
             this.panelBottom.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.simulationPanel)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +97,6 @@
         private System.Windows.Forms.Button btnStartPause;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Panel panelBottom;
-        private System.Windows.Forms.Panel simulationPanel;
+        private System.Windows.Forms.PictureBox simulationPanel;
     }
 }
--- Life/SimulationForm.cs
+++ Life/SimulationForm.cs
@@ -19,7 +19,12 @@ namespace Life
             simulation = sim;
         }
 
-        private void simulationPanel_Paint(object sender, PaintEventArgs e)
+        private void SimulationForm_Resize(object sender, EventArgs e)
+        {
+            simulationPanel.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             // In de paint functie gaan we onze objecten tekenen
             // Deze wordt aangeroepen op het moment dat het scherm wordt geresized
@@ -33,23 +38,86 @@ namespace Life
             var height = (int)Math.Floor((float)simulationPanel.Height / simulation.Context.Layout.GridSizeY);
             var width = (int)Math.Floor((float)simulationPanel.Width / simulation.Context.Layout.GridSizeX);
 
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            Brush b = Brushes.Red;
             // 2. Alle territories tekenen
+            Type tp = typeof(Brushes);
             foreach (var t in simulation.Context.Layout.Territories)
             {
-                float xPos = ((t.XPos) * width) - width;
-                float yPos = ((t.YPos) * height) - height;
-                g.FillRectangle(Brushes.Red, xPos, yPos, width, height);
+                int xPos = ((t.XPos) * width) - width;
+                int yPos = ((t.YPos) * height) - height;
+
+                rectangles.Add(new Rectangle(xPos, yPos, width, height));
+                b = (Brush)tp.GetProperty(t.Color.ToString()).GetValue(null, null);
+            }
+
+            if (rectangles.Count > 0)
+                g.FillRectangles(b, rectangles.ToArray());
+            rectangles.Clear();
+
+            // 3. Alle obstacles tekenen
+            foreach (var o in simulation.Context.GetSimObjects<LifeSimulation.SimObjects.Obstacle>().ToList())
+            {
+                int xPos = ((o.XPos) * width) - width;
+                int yPos = ((o.YPos) * height) - height;
+
+                rectangles.Add(new Rectangle(xPos, yPos, width, height));
+                b = (Brush)tp.GetProperty(o.SimObjectColor.ToString()).GetValue(null, null);
+            }
+
+            if (rectangles.Count > 0)
+                g.FillRectangles(b, rectangles.ToArray());
+            rectangles.Clear();
+
+            // 4. Alle sim objects
+            foreach (var o in simulation.Context.GetSimObjects<LifeSimulation.SimObjects.Plant>().ToList())
+            {
+                int xPos = ((o.XPos) * width) - width;
+                int yPos = ((o.YPos) * height) - height;
+
+                rectangles.Add(new Rectangle(xPos, yPos, width, height));
+                b = (Brush)tp.GetProperty(o.SimObjectColor.ToString()).GetValue(null, null);
+            }
+
+            if (rectangles.Count > 0)
+                g.FillRectangles(b, rectangles.ToArray());
+            rectangles.Clear();
+
+            // 3. Alle beesten tekenen
+            foreach (var o in simulation.Context.GetSimObjects<LifeSimulation.SimObjects.Creature>().ToList())
+            {
+                int xPos = ((o.XPos) * width) - width;
+                int yPos = ((o.YPos) * height) - height;
+
+                rectangles.Add(new Rectangle(xPos, yPos, width, height));
+                b = (Brush)tp.GetProperty(o.SimObjectColor.ToString()).GetValue(null, null);
+            }
+
+            if (rectangles.Count > 0)
+                g.FillRectangles(b, rectangles.ToArray());
+
+        }
+
+        private void btnStartPause_Click(object sender, EventArgs e)
+        {
+            if (simulation.Status == LifeSimulation.SimulationStatus.Started)
+            {
+                simulation.Pauze();
+                btnStartPause.Text = "Start";
+            }
+            else if ((simulation.Status == LifeSimulation.SimulationStatus.Pauzed)
+                || (simulation.Status == LifeSimulation.SimulationStatus.New))
+            {
+                simulation.Start();
+                btnStartPause.Text = "Pauze";
             }
         }
 
-        private void SimulationForm_Resize(object sender, EventArgs e)
+        private void btnStop_Click(object sender, EventArgs e)
         {
-            //simulationPanel.Invalidate();
-        }
-
-        private void simulationPanel_SizeChanged(object sender, EventArgs e)
-        {
-            simulationPanel.Invalidate();
+            simulation.Stop();
+            btnStartPause.Visible = false;
         }
     }
 }
